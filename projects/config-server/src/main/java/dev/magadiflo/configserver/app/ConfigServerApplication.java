2413ae17cf9bb12ad4f1e07028452f9aade139a8
Martín <magadiflo@gmail.com>
2024-05-06 14:23:10 -0500

    Chapter 5 - Construyendo nuestro servidor de configuración Spring Cloud

---
+++ projects/config-server/src/main/java/dev/magadiflo/configserver/app/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.configserver.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
