adda83eceffacce56b67dba36d057c83b9394c37
Martín <magadiflo@gmail.com>
2024-05-07 19:07:10 -0500

    Chapter 5 - Actualizando sus propiedades usando Spring Cloud Config Server

--- projects/licensing-service/src/main/java/dev/magadiflo/licensing/app/config/ServiceProperties.java
+++ projects/licensing-service/src/main/java/dev/magadiflo/licensing/app/config/ServiceProperties.java
@@ -3,9 +3,11 @@ package dev.magadiflo.licensing.app.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Getter
 @Setter
+@RefreshScope  // Indica que esta clase debe ser recargada cuando se refresca la configuración
 @ConfigurationProperties(prefix = "example")
 public class ServiceProperties {
     private String property;
