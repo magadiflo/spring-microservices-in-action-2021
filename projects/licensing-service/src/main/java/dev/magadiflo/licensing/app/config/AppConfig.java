5dc4af90e66e68d03af86980c81886eab84ce545
Martín <magadiflo@gmail.com>
2024-05-22 18:54:02 -0500

    Chapter 8 - RestTemplate personalizado y UserContextInterceptor: garantizar que el ID de correlación se propague

--- projects/licensing-service/src/main/java/dev/magadiflo/licensing/app/config/AppConfig.java
+++ projects/licensing-service/src/main/java/dev/magadiflo/licensing/app/config/AppConfig.java
@@ -1,10 +1,14 @@
 package dev.magadiflo.licensing.app.config;
 
+import dev.magadiflo.licensing.app.utils.UserContextInterceptor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -12,13 +16,19 @@ public class AppConfig {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        interceptors.add(new UserContextInterceptor());
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 
     @LoadBalanced
     @Bean
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        return RestClient.builder()
+                .requestInterceptors(clientHttpRequestInterceptors ->
+                        clientHttpRequestInterceptors.add(new UserContextInterceptor()));
     }
 
 }
---
+++ projects/licensing-service/src/main/java/dev/magadiflo/licensing/app/utils/UserContextInterceptor.java
@@ -0,0 +1,28 @@
+package dev.magadiflo.licensing.app.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+@Slf4j
+public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+            throws IOException {
+
+        HttpHeaders headers = request.getHeaders();
+        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+
+        log.debug("[Interceptor] Agregando al header tmx-correlation-id: {}", UserContextHolder.getContext().getCorrelationId());
+        log.debug("[Interceptor] Agregando al header tmx-auth-token: {}", UserContextHolder.getContext().getAuthToken());
+
+        return execution.execute(request, body);
+    }
+}
