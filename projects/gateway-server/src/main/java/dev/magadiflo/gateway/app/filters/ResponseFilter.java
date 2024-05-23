376321327f7314b81a4a9498e96358a9e55c3795
Martín <magadiflo@gmail.com>
2024-05-22 19:38:18 -0500

    Chapter 8 - 8.7 Construcción de un post-filter que recibe el ID de correlación

---
+++ projects/gateway-server/src/main/java/dev/magadiflo/gateway/app/filters/ResponseFilter.java
@@ -0,0 +1,38 @@
+package dev.magadiflo.gateway.app.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class ResponseFilter {
+
+    private final static Logger log = LoggerFactory.getLogger(ResponseFilter.class);
+
+    private final FilterUtils filterUtils;
+
+    public ResponseFilter(FilterUtils filterUtils) {
+        this.filterUtils = filterUtils;
+    }
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> {
+                    HttpHeaders headers = exchange.getRequest().getHeaders();
+                    String correlationId = this.filterUtils.getCorrelationId(headers);
+
+                    log.debug("Agregando el correlation ID a los headers salientes");
+
+                    exchange.getResponse()
+                            .getHeaders()
+                            .add(FilterUtils.CORRELATION_ID, correlationId);
+
+                    log.debug("Completando solicitud saliente para: {}", exchange.getRequest().getURI());
+                }));
+    }
+}
