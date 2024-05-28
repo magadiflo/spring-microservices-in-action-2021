39cb15b0b09641e320b9b3d394e06762daf755cc
Martín <magadiflo@gmail.com>
2024-05-28 00:40:28 -0500

    Chapter 9 - Protegiendo un servicio a través de un rol específico

--- projects/organization-service/src/main/java/dev/magadiflo/organization/app/controller/OrganizationController.java
+++ projects/organization-service/src/main/java/dev/magadiflo/organization/app/controller/OrganizationController.java
@@ -2,8 +2,10 @@ package dev.magadiflo.organization.app.controller;
 
 import dev.magadiflo.organization.app.model.Organization;
 import dev.magadiflo.organization.app.service.OrganizationService;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -15,11 +17,13 @@ public class OrganizationController {
 
     private final OrganizationService organizationService;
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping(path = "/{organizationId}")
     public ResponseEntity<Organization> getOrganization(@PathVariable String organizationId) {
         return ResponseEntity.ok(this.organizationService.findById(organizationId));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @PostMapping
     public ResponseEntity<Organization> saveOrganization(@RequestBody Organization organization) {
         Organization organizationDB = this.organizationService.create(organization);
@@ -27,11 +31,13 @@ public class OrganizationController {
         return ResponseEntity.created(location).body(organizationDB);
     }
 
+    @RolesAllowed({"ADMIN", "USER"})
     @PutMapping(path = "/{organizationId}")
     public ResponseEntity<Organization> updateOrganization(@PathVariable String organizationId, @RequestBody Organization organization) {
         return ResponseEntity.ok(this.organizationService.update(organization, organizationId));
     }
 
+    @RolesAllowed("ADMIN")
     @DeleteMapping(path = "/{organizationId}")
     public ResponseEntity<Void> deleteOrganization(@PathVariable String organizationId) {
         this.organizationService.delete(organizationId);
