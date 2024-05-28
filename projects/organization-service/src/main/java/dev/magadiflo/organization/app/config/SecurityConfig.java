f0305e6b096aaacc3cdc744e8e4915f6bc4a8032
Martín <magadiflo@gmail.com>
2024-05-27 20:41:06 -0500

    Chapter 9 - Sobreescribiendo o reemplazando la auto-configuración de arranque

---
+++ projects/organization-service/src/main/java/dev/magadiflo/organization/app/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package dev.magadiflo.organization.app.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * The prePostEnabled property enables Spring Security pre/post annotations.
+ * The securedEnabled property determines if the @Secured annotation should be enabled.
+ * The jsr250Enabled property allows us to use the @RolesAllowed annotation.
+ */
+
+@EnableMethodSecurity(jsr250Enabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(this.jwtAuthConverter())));
+        return http.build();
+    }
+
+    // Convertir los roles de keycloak en una representación compatible con spring security
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return converter;
+    }
+}
+
+@Slf4j
+@SuppressWarnings("unchecked")
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        if (source.getClaims() == null) {
+            return List.of();
+        }
+        final Map<String, Map<String, List<String>>> resourceAccess = (Map<String, Map<String, List<String>>>) source.getClaims().get("resource_access");
+        final Map<String, List<String>> ostock = (Map<String, List<String>>) resourceAccess.get("ostock");
+        return ostock.get("roles").stream()
+                .map(roleName -> {
+                    log.info("roleName: {}", roleName);
+                    return "ROLE_" + roleName;
+                })
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
+
